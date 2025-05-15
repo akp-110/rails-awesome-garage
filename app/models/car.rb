304b81f277d2a1afc4857208a880080fa970d625
Anil Patel <patelak1@gmail.com>
2025-05-15 10:55:53 +0100

    fixing typos in car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :owner
 
-  vaildates :brand, presence: true
+  validates :brand, presence: true
   validates :model, presence: true
   validates :year, numericality: { only_integer: true }
-  vaildates :fuel, presence: true
+  validates :fuel, presence: true
 end
