4d977dfe11ac9fc749c549815f67c827ee25ad67
Anil Patel <patelak1@gmail.com>
2025-05-15 11:14:15 +0100

    seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@
 #
 # Example:
 #
-#   ['Action', 'Comedy', 'Drama', 'Horror'].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
+#   ['Action', 'Comedy', 'Drama', 'Horror'].each do |genre_|
+#     MovieGenre.find_or_create_by!(: genre_)
 #   end
 puts 'Seeding data......'
 
@@ -13,39 +13,50 @@ Car.destroy_all
 Review.destroy_all
 Owner.destroy_all
 
+owner1 = Owner.create!(nickname: 'Jean Phillpe-Mateta ')
+owner2 = Owner.create!(nickname: 'Pep Guardiola')
+owner3 = Owner.create!(nickname: 'Erling Haaland')
+owner4 = Owner.create!(nickname: 'Adam Wharton')
+owner5 = Owner.create!(nickname: 'Kevin de Bruyne')
+
 Car.create!(
   brand: 'Porsche',
   model: '911 GT3 RS 992',
   year: '2023',
-  fuel: 'Unleaded petrol'
+  fuel: 'Unleaded petrol',
+  owner: owner1
 )
 
 Car.create!(
   brand: 'Ford',
   model: 'Fiesta',
   year: '2022',
-  fuel: 'Unleaded petrol'
+  fuel: 'Unleaded petrol',
+  owner: owner2
 )
 
 Car.create!(
   brand: 'Volkswagen',
   model: 'Golf GTI',
   year: '2024',
-  fuel: 'Unleaded petrol'
+  fuel: 'Unleaded petrol',
+  owner: owner3
 )
 
 Car.create!(
   brand: 'Bugatti',
   model: 'Chiron',
   year: '2016',
-  fuel: 'Unleaded petrol'
+  fuel: 'Unleaded petrol',
+  owner: owner4
 )
 
 Car.create!(
   brand: 'Lamborghini',
   model: 'SV Roadster',
   year: '2017',
-  fuel: 'Unleaded petrol'
+  fuel: 'Unleaded petrol',
+  owner: owner5
 )
 
-puts "#{Car.count} cars created"
+puts "#{Owner.count} owners created and #{Car.count} cars created"
